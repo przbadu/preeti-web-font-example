21a6bbb1b06a43bd10409f1655935ed48bcb9432
przbadu <pushparaj.badu@gmail.com>
2017-06-13 14:49:38 +0545

    Add users saffold and update code to use nepali translation code

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612041103) do
+ActiveRecord::Schema.define(version: 20170613084158) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "age"
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
